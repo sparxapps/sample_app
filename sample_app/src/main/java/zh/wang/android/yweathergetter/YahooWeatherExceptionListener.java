ff25019ae97326e61a57c40f436e088e03bd0bb7
sis248 <lalit.sharma@sparsitsolutions.com>
2016-05-18 14:51:47 +0530

    Initial commit

---
+++ sample_app/src/main/java/zh/wang/android/yweathergetter/YahooWeatherExceptionListener.java
@@ -0,0 +1,7 @@
+package zh.wang.android.yweathergetter;
+
+public interface YahooWeatherExceptionListener {
+    public void onFailConnection(final Exception e);
+    public void onFailParsing(final Exception e);
+    public void onFailFindLocation(final Exception e);
+}
