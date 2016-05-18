49679f0bffd73080c39d6f7e5a5e6ff2abae246c
sis248 <lalit.sharma@sparsitsolutions.com>
2016-05-18 14:58:06 +0530

    added comment

--- sample_app/src/main/java/zh/wang/android/yweathergetter/MainActivity.java
+++ sample_app/src/main/java/zh/wang/android/yweathergetter/MainActivity.java
@@ -33,7 +33,7 @@ import android.widget.Toast;
 
 import static zh.wang.android.yweathergetter.YahooWeather.*;
 
-
+//added by Lalit Sharma
 public class MainActivity extends Activity implements YahooWeatherInfoListener,
     YahooWeatherExceptionListener {
 	
